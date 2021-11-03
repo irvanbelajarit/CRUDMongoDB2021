5e614aff4622c7499ee3f7851a21e35566d87f72
irvanln <irvina22@gmail.com>
2021-11-03 08:00:38 +0700

    CRUD MongoDb

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//method override
+const methodOverride = require("method-override");
+
+//require session cookies
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+
+//configurasi connect flash message
+app.use(cookieParser("secrect"));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+//use method override
+app.use(methodOverride("_method"));
+
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -18,6 +41,7 @@ const Contact = require("./model/contact");
 //validasi express validator
 const { body, check, validationResult } = require("express-validator");
 const { rawListeners } = require("./model/contact");
+const { isValidObjectId } = require("mongoose");
 
 app.get("/", (req, res) => {
   res.render("index", { title: "halaman home", layout: "layouts/main-layout" });
@@ -30,6 +54,7 @@ app.get("/contact", async (req, res) => {
     title: "contact ejs",
     contacts,
     layout: "layouts/main-layout",
+    msg: req.flash("msg"), //untuk menampilkan data pesan
   });
 });
 
@@ -63,6 +88,7 @@ app.post(
       });
     } else {
       Contact.insertMany(req.body, (error, result) => {
+        req.flash("msg", "Data Berhasil Ditambahkan!"); //untuk menambahkan flash message
         res.redirect("/contact");
       });
     }
@@ -79,8 +105,47 @@ app.get("/contact/edit/:id", async (req, res) => {
   });
 });
 
-app.post(
-  "/contact/update",
+// app.post(
+//   "/contact/update",
+//   [
+//     body("nama").custom(async (value) => {
+//       const duplikat = await Contact.findOne({ nama: value });
+//       if (duplikat) {
+//         throw new Error("nama contact sudah digunakan!");
+//       }
+//       return true;
+//     }),
+//     check("email", "Email tidak valid").isEmail(),
+//     check("nohp", "No HP tidak valid!").isMobilePhone("id-ID"),
+//   ],
+//   (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       res.render("tambah-data", {
+//         title: "form Edit data kontak",
+//         layout: "layouts/main-layout",
+//         errors: errors.array(),
+//       });
+//     } else {
+//       Contact.updateOne(
+//         { _id: req.body._id },
+//         {
+//           $set: {
+//             nama: req.body.nama,
+//             email: req.body.email,
+//             nohp: req.body.nohp,
+//           },
+//         }
+//       ).then((result) => {
+//         req.flash("msg", "Data kontak berhasil Diubah!");
+//         res.redirect("/contact");
+//       });
+//     }
+//   }
+// );
+
+app.put(
+  "/contact",
   [
     body("nama").custom(async (value) => {
       const duplikat = await Contact.findOne({ nama: value });
@@ -95,17 +160,14 @@ app.post(
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.render("edit-data", {
+      res.render("tambah-data", {
         title: "form Edit data kontak",
         layout: "layouts/main-layout",
         errors: errors.array(),
       });
     } else {
-      console.log(req.body);
       Contact.updateOne(
-        {
-          _id: req.body._id,
-        },
+        { _id: req.body._id },
         {
           $set: {
             nama: req.body.nama,
@@ -114,14 +176,24 @@ app.post(
           },
         }
       ).then((result) => {
+        req.flash("msg", "Data kontak berhasil Diubah!");
         res.redirect("/contact");
       });
     }
   }
 );
 
-app.get("/contact/delete/:id", (req, res) => {
-  Contact.findByIdAndDelete(req.params.id).then((result) => {
+// app.get("/contact/delete/:id", (req, res) => {
+//   Contact.findByIdAndDelete(req.params.id).then((result) => {
+//     req.flash("msg", "Data Berhasil Dihapus!"); //untuk menambahkan flash message
+
+//     res.redirect("/contact");
+//   });
+// });
+
+app.delete("/contact", (req, res) => {
+  Contact.findByIdAndDelete(req.body.id).then((result) => {
+    req.flash("msg", "Data Berhasil Dihapus!"); //untuk menambahkan flash message
     res.redirect("/contact");
   });
 });
--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/crud_untar2021");
+mongoose.connect("mongodb://127.0.0.1:27017/crud_untar2021", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 // //tambah data
 // const contact1 = new Contact({
